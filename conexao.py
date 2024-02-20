422b83a6e476d9cedd36190aed944a95a77f419f
Fabricio Caixeta Macedo <2202294@SALAS.AULAS>
2024-02-19 22:07:11 -0300

    conexao pymysql

---
+++ conexao.py
@@ -0,0 +1,20 @@
+import pymysql
+hostname = 'database1.c3awyky0axj2.us-west-2.rds.amazonaws.com'
+user = 'admin'
+password = 'admin3311'
+
+db = pymysql.connections.Connection(
+    host=hostname,
+    user=user,
+    password=password
+)
+
+cursor = db.cursor()
+
+cursor.execute("CREATE DATABASE IF NOT EXISTS notebooks_db_fabricio")
+cursor.execute("SHOW DATABASES")
+for databases in cursor:
+    print(databases)
+
+cursor.close()
+db.close()
